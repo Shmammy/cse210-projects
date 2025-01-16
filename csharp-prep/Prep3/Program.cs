64b48dc8980a5b3f4bffebb41e496d33712c2eab
Sam Waters <sam.waters.jacobsen@gmail.com>
2025-01-16 16:09:37 -0700

    Created magic number and guess inputs. If statements for 'Higher' and 'Lower'.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Runtime.Versioning;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Magic Number: ");
+        string magicNumberInput = Console.ReadLine();
+        int magicNumber = int.Parse(magicNumberInput);
+        int guess = 0;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+        } while (guess != magicNumber);
+
+        Console.WriteLine("You guessed it!");
     }
 }
