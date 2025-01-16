272e374c3845d53f56a439522f58a68464b07a37
Sam Waters <sam.waters.jacobsen@gmail.com>
2025-01-16 16:59:41 -0700

    Get input from user to create list. Find sum of list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,26 @@
-using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int numberInput = 1;
+        
+        while (numberInput != 0)
+        {
+            Console.Write("Enter number: ");
+            numberInput = int.Parse(Console.ReadLine());
+            if (numberInput != 0)
+            {
+                numbers.Add(numberInput);
+            }
+        }
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
